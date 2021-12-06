23cc6bcaef92d4cc5ca309377ad3e0253b90e969
Marina Robleño <marinarsanisidro@gmail.com>
2021-12-06 12:40:45 +0100

    Added Order By action to Bookings slice

--- src/components/BookingList.jsx
+++ src/components/BookingList.jsx
@@ -2,8 +2,28 @@ import React from "react";
 import Button from "./Button";
 import { AiOutlineDelete } from "react-icons/ai";
 import styled from "styled-components";
-import { remove, selectBookings } from "../features/slices/bookingsSlice";
-import { useSelector, useDispatch } from 'react-redux';
+import { remove, orderBy, selectBookings } from "../features/slices/bookingsSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { useState } from "react";
+
+export const StyledFilterHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 35px;
+`;
+
+export const StyledFilterMenu = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+export const StyledMenuItem = styled.div`
+  padding: 13px 26px;
+  border-bottom: 1px solid ${(props) => props.theme.colors.border_grey_light_5};
+  cursor: pointer;
+`;
+
+export const StyledSelect = styled.select``;
 
 export const StyledTable = styled.table`
   text-align: left;
@@ -16,7 +36,6 @@ export const StyledTable = styled.table`
 `;
 
 export const StyledHeader = styled.tr`
-  
   font: normal normal 600 16px/25px Poppins;
 `;
 
@@ -27,49 +46,79 @@ export const StyledData = styled.tr`
 `;
 
 export function BookingList() {
-  const myBooking = useSelector(selectBookings)
+  const myBooking = useSelector(selectBookings);
   const dispatch = useDispatch();
 
   const removeBooking = (book) => {
     dispatch(remove(book));
-    
-}
+  };
 
+  const [select, setSelect] = useState("");
+
+  const handleNewOldSelect = (e) => {
+    e.preventDefault();
+    const newSelect = e.target.value;
+    setSelect(newSelect);
+    dispatch(orderBy(newSelect));
+  };
 
   return (
-    <StyledTable>
-      <StyledHeader>
-        <th class="header-table-sector">Guest</th>
-        <th class="header-table-sector">Order date</th>
-        <th class="header-table-sector">Check in</th>
-        <th class="header-table-sector">Check out</th>
-        <th class="header-table-sector">Special Request</th>
-        <th class="header-table-sector">Room Type</th>
-        <th class="header-table-sector">Status</th>
-      </StyledHeader>
-      {myBooking.map((book) => (
-        <StyledData>
-          <td className="data-element">
-            <div>{book.guest}</div>
-            {book.id}
-          </td>
-          <td className="data-element">{book.orderDate}</td>
-          <td className="data-element">{book.checkIn}</td>
-          <td className="data-element">{book.checkOut}</td>
-          <td className="data-element">
-            <Button notes>View Notes</Button>
-          </td>
-          <td className="data-element">{book.roomType}</td>
-          <td className="data-element">
-            <Button checkIn name="Check In" >
-              Check In
-            </Button>
-          </td>
-          <td className="data-element">
-            <AiOutlineDelete onClick={() => removeBooking(book)} style={{ cursor: "pointer" }}  />
-          </td>
-        </StyledData>
-      ))}
-    </StyledTable>
+    <div style={{ display: "flex", flexDirection: "column", width: "100%" }}>
+      <StyledFilterHeader>
+        <StyledFilterMenu>
+          <StyledMenuItem>All Bookings</StyledMenuItem>
+          <StyledMenuItem>Checking In</StyledMenuItem>
+          <StyledMenuItem>Checking Out</StyledMenuItem>
+          <StyledMenuItem>In Progress</StyledMenuItem>
+        </StyledFilterMenu>
+        <div>
+          <StyledSelect>Date</StyledSelect>
+          <StyledSelect value={select} onChange={handleNewOldSelect}>
+            <option selected>Order By...</option>
+            <option value={"newest"}>
+              Newest
+            </option>
+            <option value={"oldest"}>Oldest</option>
+          </StyledSelect>
+        </div>
+      </StyledFilterHeader>
+      <StyledTable>
+        <StyledHeader>
+          <th class="header-table-sector">Guest</th>
+          <th class="header-table-sector">Order date</th>
+          <th class="header-table-sector">Check in</th>
+          <th class="header-table-sector">Check out</th>
+          <th class="header-table-sector">Special Request</th>
+          <th class="header-table-sector">Room Type</th>
+          <th class="header-table-sector">Status</th>
+        </StyledHeader>
+        {myBooking.map((book) => (
+          <StyledData>
+            <td className="data-element">
+              <div>{book.guest}</div>
+              {book.id}
+            </td>
+            <td className="data-element">{book.orderDate}</td>
+            <td className="data-element">{book.checkIn}</td>
+            <td className="data-element">{book.checkOut}</td>
+            <td className="data-element">
+              <Button notes>View Notes</Button>
+            </td>
+            <td className="data-element">{book.roomType}</td>
+            <td className="data-element">
+              <Button checkIn name="Check In">
+                Check In
+              </Button>
+            </td>
+            <td className="data-element">
+              <AiOutlineDelete
+                onClick={() => removeBooking(book)}
+                style={{ cursor: "pointer" }}
+              />
+            </td>
+          </StyledData>
+        ))}
+      </StyledTable>
+    </div>
   );
 }
--- src/features/slices/bookingsSlice.js
+++ src/features/slices/bookingsSlice.js
@@ -9,11 +9,35 @@ export const bookingsSlice = createSlice({
             state = state.filter(book => book.id !== action.payload.id);
             return state;
         },
+        orderBy: (state, action) => {
+            if (action.payload){
+                action.payload === 'newest' ?
+                state = state.sort((a,b) => {
+                        if (a.orderDate > b.orderDate){
+                            return -1;
+                        }
+                        if(a.orderDate < b.orderDate){
+                            return 1;
+                        }
+                        return 0;
+                }) : state = state.sort((a,b) => {
+                    if (a.orderDate > b.orderDate){
+                        return 1;
+                    }
+                    if(a.orderDate < b.orderDate){
+                        return -1;
+                    }
+                    return 0;
+            })
+            }
+            
+            
+        }
     }
 })
 
 export const selectBookings = (state) => state.bookings;
 
-export const { remove } = bookingsSlice.actions
+export const { remove, orderBy } = bookingsSlice.actions
 
 export default bookingsSlice.reducer;
