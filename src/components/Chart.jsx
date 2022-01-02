e8e665caa6a8e691ddd549cf4b8667a684a7d447
Marina Robleño <marinarsanisidro@gmail.com>
2022-01-02 23:47:53 +0100

    Pulled master at laptop

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-import * as d3 from "d3";
+import * as d3 from 'd3'
 
 export default function ReservationChart() {
   const [checkIn, setCheckIn] = useState([20, 3, 20, 17, 10, 39, 12]);
